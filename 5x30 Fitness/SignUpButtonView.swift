b5fb38c966672b9db2d73f059dda66cafc4b0d4d
noBthd <Egorkir0401@gmail.com>
2025-05-09 23:41:09 +0300

    added sign up button view

---
+++ 5x30 Fitness/SignUpButtonView.swift
@@ -0,0 +1,47 @@
+//
+//  SignUpButtonView.swift
+//  5x30 Fitness
+//
+//  Created by Егор Кириченко on 09.05.2025.
+//
+
+import SwiftUI
+
+struct SignUpButton: View {
+    let SignUpTitle: String
+    let SignUp: () -> Void
+    
+    var body: some View {
+        VStack (spacing: 10){
+            // SIGN UP BUTTON
+            Button(action: SignUp){
+                Text(SignUpTitle)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 280.0, height: 24.0)
+                    .foregroundStyle(Color.white)
+                    .padding(.vertical, 9.0)
+                    .padding(.horizontal, 10)
+                    .font(
+                        Font.custom(
+                            "Myanmar Khyay",
+                            size: 18
+                        )
+                    )
+            }
+            .background(Color("baseColor"))
+            .cornerRadius(12)
+        }
+    }
+}
+
+//TODO: rplace this func with registration
+func someSignUp() -> Void {
+    print("USER SIGNED IN")
+}
+
+#Preview{
+    SignUpButton(
+        SignUpTitle: "Sign Up",
+        SignUp: someSignIn
+    )
+}
