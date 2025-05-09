5aab9d7c463110b6d8fe491d85caaffd8730706d
noBthd <Egorkir0401@gmail.com>
2025-05-09 22:16:17 +0300

    initial commit

---
+++ 5x30 Fitness/_x30_FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  _x30_FitnessApp.swift
+//  5x30 Fitness
+//
+//  Created by Егор Кириченко on 09.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct _x30_FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 5x30 FitnessTests/_x30_FitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  _x30_FitnessTests.swift
+//  5x30 FitnessTests
+//
+//  Created by Егор Кириченко on 09.05.2025.
+//
+
+import Testing
+@testable import _x30_Fitness
+
+struct _x30_FitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
